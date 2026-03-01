d1d60e2eca492e8eae794a4fb1a68e826de34836
YourUsername <leojosefm@gmail.com>
2026-03-01 16:40:27 +0000

    dafs

--- mwaa/docker/script/verification.sh
+++ mwaa/docker/script/verification.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-EXPECTED_AIRFLOW_VERSION="2.6.3"
-EXPECTED_WATCHTOWER_VERSION="2.0.1"
+EXPECTED_AIRFLOW_VERSION="2.10.3"
+EXPECTED_WATCHTOWER_VERSION="3.3.1"
 
 validate_version_with_pip() {
   if [[ $(pip3 show "$1" | grep 'Version' | grep -o '[0-9].*') != "$2" ]]
@@ -10,9 +10,9 @@ validate_version_with_pip() {
   fi
 }
 
-if ! python3 -c 'import sys; assert sys.version_info[:2] == (3,10)' > /dev/null;
+if ! python3 -c 'import sys; assert sys.version_info[:2] == (3,11)' > /dev/null;
 then
-  echo "Validation Error: Python version 3.10 expected"
+  echo "Validation Error: Python version 3.11 expected"
 fi
 
 validate_version_with_pip "apache-airflow" "$EXPECTED_AIRFLOW_VERSION"
